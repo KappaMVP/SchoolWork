9cef0f74b6497ec37a4171c7d521233367f60a03
cookies <C107156108@nkust.edu.tw>
2020-12-28 16:51:36 +0800

    ㄚ哲要的啦

--- src/Pages/NotifyPage.js
+++ src/Pages/NotifyPage.js
@@ -1,12 +1,15 @@
 //通知頁
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import NotifyView from '../Views/NotifyView';
 import {notifyPageData as pageData} from '../data.source';
-
+// import ScrollableTabView, {
+//   DefaultTabBar,
+// } from 'react-native-scrollable-tab-view';
+// import Icon from '../Views/Elements/Icon';
 class NotifyPage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   componentDidMount() {
@@ -17,11 +20,43 @@ class NotifyPage extends React.Component {
 
   render() {
     return (
-      <View>
-        <Text> NotifyPage </Text>
-      </View>
+      // <View style={styles.main}>
+      //   <ScrollableTabView
+      //     tabBarPosition="top"
+      //     locked={false}
+      //     tabBarUnderlineStyle="gray"
+      //     renderTabBar={() => <DefaultTabBar />}>
+      //     <Text style={styles.textStyles} tabLabel="讚">
+      //       Like
+      //     </Text>
+      //     <Text style={styles.textStyles} tabLabel="留言">
+      //       Message
+      //     </Text>
+      //     <Text style={styles.textStyles} tabLabel="被標註">
+      //       Mention
+      //     </Text>
+      //   </ScrollableTabView>
+      // </View>
+      <View></View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  textStyles: {
+    flex: 1,
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  main: {
+    flex: 1,
+  },
+  mainNotice: {
+    flex: 0.3,
+  },
+  noticeContent: {
+    flex: 0.7,
+  },
+});
 
 export default NotifyPage;
