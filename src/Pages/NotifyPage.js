01d4d95ef0b4eadaa1d0b3c08d26db3f0a9e0fb8
cookies <C107156108@nkust.edu.tw>
2020-12-28 19:03:30 +0800

    cookie insert

--- src/Pages/NotifyPage.js
+++ src/Pages/NotifyPage.js
@@ -1,16 +1,41 @@
 //通知頁
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet, Image} from 'react-native';
 import NotifyView from '../Views/NotifyView';
 import {notifyPageData as pageData} from '../data.source';
-// import ScrollableTabView, {
-//   DefaultTabBar,
-// } from 'react-native-scrollable-tab-view';
-// import Icon from '../Views/Elements/Icon';
 class NotifyPage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  fakes = [
+    {
+      id: 1,
+      userName: '美國隊長',
+      userPhoto: 'https://picsum.photos/600/300?random=5',
+      status: 'Like your post.',
+    },
+    {
+      id: 2,
+      userName: '奇異博士',
+      userPhoto: 'https://picsum.photos/600/300?random=21',
+      status: 'Leave a message on your post.',
+    },
+    {
+      id: 3,
+      userName: '蜘蛛人',
+      userPhoto: 'https://picsum.photos/600/300?random=6',
+      status: 'Tagged you on other people post.',
+    },
+    {
+      id: 4,
+      userName: '黑寡婦',
+      userPhoto: 'https://picsum.photos/600/300?random=28',
+      status: 'Leave a message on your post.',
+    },
+    {
+      id: 5,
+      userName: '雷神索爾',
+      userPhoto: 'https://picsum.photos/600/300?random=23',
+      status: 'Like your post.',
+    },
+  ];
 
   componentDidMount() {
     this.props.navigation.setParams({
@@ -20,43 +45,13 @@ class NotifyPage extends React.Component {
 
   render() {
     return (
-      // <View style={styles.main}>
-      //   <ScrollableTabView
-      //     tabBarPosition="top"
-      //     locked={false}
-      //     tabBarUnderlineStyle="gray"
-      //     renderTabBar={() => <DefaultTabBar />}>
-      //     <Text style={styles.textStyles} tabLabel="讚">
-      //       Like
-      //     </Text>
-      //     <Text style={styles.textStyles} tabLabel="留言">
-      //       Message
-      //     </Text>
-      //     <Text style={styles.textStyles} tabLabel="被標註">
-      //       Mention
-      //     </Text>
-      //   </ScrollableTabView>
-      // </View>
-      <View></View>
+      <View>
+        {this.fakes.map(function (fake) {
+          return <NotifyView key={fake.id} fake={fake} />;
+        })}
+      </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  textStyles: {
-    flex: 1,
-    fontSize: 20,
-    textAlign: 'center',
-  },
-  main: {
-    flex: 1,
-  },
-  mainNotice: {
-    flex: 0.3,
-  },
-  noticeContent: {
-    flex: 0.7,
-  },
-});
 
 export default NotifyPage;
--- src/Views/NotifyView.js
+++ src/Views/NotifyView.js
@@ -1,1 +1,33 @@
 //通知的橫列
+import React from 'react';
+import {StyleSheet, View, Text, Image} from 'react-native';
+
+export default function NotifyView(props) {
+  return (
+    <View style={Styles.content}>
+      <Image source={{uri: props.fake.userPhoto}} />
+      <View>
+        <Text style={Styles.name}>{props.fake.userName}</Text>
+      </View>
+      <Text>{props.fake.status}</Text>
+    </View>
+  );
+}
+
+const Styles = StyleSheet.create({
+  content: {
+    flexDirection: 'row', // 每個 TodoItem 區塊透過水平方向排列
+    justifyContent: 'space-between', // 區塊貼齊左右兩邊
+    alignItems: 'center', // 垂直置中
+    marginVertical: 10, // 區塊上下垂直外距大小
+    padding: 10, // 區塊四周內距大小
+  },
+  name: {
+    fontSize: 20, // 標題文字大小
+    fontWeight: 'bold', // 標題文字粗細
+  },
+  subTitle: {
+    fontSize: 14, // 子標題文字大小
+    color: 'gray', // 子標題文字顏色
+  },
+});
