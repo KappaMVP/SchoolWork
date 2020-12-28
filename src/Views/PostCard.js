3e03d467e0ac269d9f4698699351cf42d5ed757a
meiqq <52100376+meiqq@users.noreply.github.com>
2020-12-28 18:19:44 +0800

    meiya.

--- src/Views/PostCard.js
+++ src/Views/PostCard.js
@@ -1,12 +1,13 @@
 //主頁顯示的簡易貼文
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image, View, Text} from 'react-native';
 import Styles from '../Styles/PostCard.style';
 
 function PostCard(props) {
   const {name, content} = props.item;
   return (
     <View style={Styles.view}>
+      <Image source={{uri: ''}} />
       <Text>{name}</Text>
       <Text>{content}</Text>
     </View>
