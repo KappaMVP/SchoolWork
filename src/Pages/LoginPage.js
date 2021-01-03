42af70bb5345759ff91d58b9b109b9285df689b3
lulusabee <lulusabeetw@gmail.com>
2021-01-03 17:01:44 +0800

    login

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,16 +1,29 @@
 //登入
 import React from 'react';
 import {View, Text} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
-function LoginPage() {
-  return (
-    <View>
-      <Text>LoginPage</Text>
-      <Text>電子郵件登入</Text>
-      <Text>google</Text>
-      <Text>facebook</Text>
-    </View>
-  );
+class LoginPage extends React.Component {
+  //sign in
+  // auth()
+  //   .signInWithEmailAndPassword('lours288300@gmail.com', 'aa890521')
+  //   .then(() => {
+  //     console.log('User account created & signed in!');
+  //   });
+
+  //sign out
+  // auth()
+  //   .signOut()
+  //   .then(() => console.log('User signed out!'));
+  render() {
+    return (
+      <View>
+        <View>
+          <Text>Log In</Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default LoginPage;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable curly */
+/* eslint-disable no-shadow */
+/* eslint-disable react-hooks/rules-of-hooks */
 //集成
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View} from 'react-native';
 import Styles from './Styles/Page.style';
 import MainRouter from './routers/MainRouter';
 import LoginRouter from './routers/LoginRouter';
+import auth from '@react-native-firebase/auth';
 
 //隱藏WARN的log
 import {LogBox} from 'react-native';
@@ -11,11 +16,27 @@ LogBox.ignoreLogs(['']);
 //因react-native-scrollable-tab-view 底層引發 getNode 跟 unit key 的警告
 
 function index() {
+  // Set an initializing state whilst Firebase connects
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (initializing) return null;
+  console.log('--------------');
+  console.log(user);
+  console.log('--------------');
   return (
-    <View style={Styles.page}>
-      {/* <LoginRouter /> */}
-      <MainRouter />
-    </View>
+    <View style={Styles.page}>{!user ? <LoginRouter /> : <MainRouter />}</View>
   );
 }
 export default index;
