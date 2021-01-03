ba1087074497d4bfdaedfd73c724a5671bb0210d
meiqq <52100376+meiqq@users.noreply.github.com>
2021-01-03 16:39:27 +0800

    下次貼文繼續排版！

--- src/Styles/PostCard.style.js
+++ src/Styles/PostCard.style.js
@@ -8,16 +8,48 @@ export default StyleSheet.create({
     backgroundColor: 'floralwhite',
   },
   cardImage: {
+    flex: 0.8,
     borderWidth: 1,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
-  cardText: {
+  image: {
+    height: '100%',
+    width: '100%',
+  },
+  cardLike: {
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  textLike: {},
+  cardName: {
     borderWidth: 1,
     justifyContent: 'flex-start',
   },
-  image: {
-    height: '80%',
-    width: '100%',
+  textName: {},
+  cardContent: {
+    borderWidth: 1,
+  },
+  textContent: {},
+
+  cardLabel: {
+    flexDirection: 'row',
+    borderWidth: 1,
+  },
+  textLabel: {
+    color: 'gray',
+  },
+  cardModel: {
+    flexDirection: 'row',
+    borderWidth: 1,
+  },
+  textModel: {
+    color: 'gray',
+  },
+  cardTime: {
+    borderWidth: 1,
+  },
+  textTime: {
+    color: 'black',
   },
 });
--- src/Views/PostCard.js
+++ src/Views/PostCard.js
@@ -4,15 +4,40 @@ import {Image, View, Text} from 'react-native';
 import Styles from '../Styles/PostCard.style';
 
 function PostCard(props) {
-  const {name, content, photo} = props.item;
+  const {name, photo, who_like, time, label, model} = props.item;
   return (
     <View style={Styles.container}>
+      {/* 圖片 */}
       <View style={Styles.cardImage}>
         <Image style={Styles.image} source={{uri: photo}} />
       </View>
-      <View style={Styles.cardText}>
+      {/* 貼文讚數 map */}
+      <View style={Styles.cardLike}>
+        <Text>目前有</Text>
+        {who_like.map((like) => (
+          <Text> {like}</Text>
+        ))}
+        <Text> 按讚</Text>
+      </View>
+      {/* 貼文者姓名 */}
+      <View style={Styles.cardName}>
         <Text>{name}</Text>
-        <Text>{content}</Text>
+      </View>
+      {/* 貼文標籤 map */}
+      <View style={Styles.cardLabel}>
+        {label.map((label) => (
+          <Text style={Styles.textLabel}>#{label} </Text>
+        ))}
+      </View>
+      {/* 貼文標記麻豆 map */}
+      <View style={Styles.cardModel}>
+        {model.map((model) => (
+          <Text style={Styles.textModel}>#{model} </Text>
+        ))}
+      </View>
+      {/* 貼文時間 */}
+      <View style={Styles.cardTime}>
+        <Text sytle={Styles.textTime}>{time}</Text>
       </View>
     </View>
   );
