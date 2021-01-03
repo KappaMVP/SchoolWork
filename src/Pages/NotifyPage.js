16a90baeee124cd0f19e50ec9fc4ec8496bd503b
cookies <C107156108@nkust.edu.tw>
2021-01-03 18:28:00 +0800

    改個人頁面跟通知陳列方式

--- src/Pages/NotifyPage.js
+++ src/Pages/NotifyPage.js
@@ -1,6 +1,6 @@
 //通知頁
 import React from 'react';
-import {View, StyleSheet, Image} from 'react-native';
+import {View, StyleSheet, Text, Image, ScrollView} from 'react-native';
 import NotifyView from '../Views/NotifyView';
 import {notifyPageData as pageData} from '../data.source';
 class NotifyPage extends React.Component {
@@ -8,32 +8,32 @@ class NotifyPage extends React.Component {
     {
       id: 1,
       userName: '美國隊長',
-      userPhoto: 'https://picsum.photos/600/300?random=5',
       status: 'Like your post.',
+      time: '2020-12-25 12:20',
     },
     {
       id: 2,
       userName: '奇異博士',
-      userPhoto: 'https://picsum.photos/600/300?random=21',
       status: 'Leave a message on your post.',
+      time: '2020-12-26 13:20',
     },
     {
       id: 3,
       userName: '蜘蛛人',
-      userPhoto: 'https://picsum.photos/600/300?random=6',
       status: 'Tagged you on other people post.',
+      time: '2020-12-27 14:20',
     },
     {
       id: 4,
       userName: '黑寡婦',
-      userPhoto: 'https://picsum.photos/600/300?random=28',
       status: 'Leave a message on your post.',
+      time: '2020-12-28 15:20',
     },
     {
       id: 5,
       userName: '雷神索爾',
-      userPhoto: 'https://picsum.photos/600/300?random=23',
       status: 'Like your post.',
+      time: '2020-12-29 16:20',
     },
   ];
 
@@ -45,13 +45,53 @@ class NotifyPage extends React.Component {
 
   render() {
     return (
-      <View>
-        {this.fakes.map(function (fake) {
-          return <NotifyView key={fake.id} fake={fake} />;
-        })}
+      <View style={NPStyles.container}>
+        <ScrollView>
+          <Text style={NPStyles.title}>本週</Text>
+          <View>
+            {this.fakes.map((fake) => (
+              <NotifyView key={fake.id} fake={fake} />
+            ))}
+          </View>
+          <Text style={NPStyles.title}>本月</Text>
+          {this.fakes.map((fake) => (
+            <NotifyView key={fake.id} fake={fake} />
+          ))}
+          <Text style={NPStyles.title}>更早之前</Text>
+          {this.fakes.map((fake) => (
+            <NotifyView key={fake.id} fake={fake} />
+          ))}
+        </ScrollView>
       </View>
     );
   }
 }
+
+const NPStyles = StyleSheet.create({
+  container: {
+    flex: 1, // 分割畫面區塊
+    backgroundColor: '#FFF', // 版面背景顏色
+  },
+  title: {
+    textAlign: 'left', // 標題文字置中
+    fontWeight: 'bold',
+    fontSize: 20, // 標題文字大小
+    padding: 10,
+  },
+  divider: {
+    borderWidth: 0.5,
+    borderColor: '#DDDDDD',
+    paddingVertical: 10,
+  },
+  todoTitle: {
+    color: '#5661EC',
+    fontSize: 20, // 標題文字大小
+    fontWeight: 'bold', // 標題文字粗細
+    padding: 10, // 上下垂直內聚大小
+  },
+  todoItems: {
+    marginHorizontal: 10, // TodoItems 整個區塊的左右外距大小
+  },
+});
 
 export default NotifyPage;
--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,6 +1,5 @@
-//個人檔案
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image, StyleSheet, ScrollView} from 'react-native';
 import {profilePageData as pageData} from '../data.source';
 import Styles from '../Styles/ProfilePage.style';
 import ScrollableTabView, {
@@ -8,8 +7,75 @@ import ScrollableTabView, {
 } from 'react-native-scrollable-tab-view';
 
 class ProfilePage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      meals: [
+        {
+          id: 1,
+          name: '嫩煎魚排佐鮮蔬',
+          price: 350,
+          url:
+            'https://live.staticflickr.com/65535/49983611666_f329a3c368_b.jpg',
+          like: '1',
+        },
+        {
+          id: 2,
+          name: '鮮蝦河粉',
+          price: 280,
+          url:
+            'https://live.staticflickr.com/65535/49997526092_91d7cf0963_b.jpg',
+          like: '3',
+        },
+        {
+          id: 3,
+          name: '清炒番茄義大利麵',
+          price: 180,
+          url:
+            'https://live.staticflickr.com/65535/49997272991_42d8072cf7_b.jpg',
+          like: '2',
+        },
+        {
+          id: 4,
+          name: '九層塔沙拉',
+          price: 120,
+          url: 'https://i.imgur.com/Yg1t5sW.jpg',
+        },
+        {
+          id: 5,
+          name: '123',
+          price: 120,
+          url:
+            'https://live.staticflickr.com/65535/49983613426_29e5108012_b.jpg',
+          like: '1',
+        },
+        {
+          id: 6,
+          name: '457',
+          price: 120,
+          url:
+            'https://live.staticflickr.com/65535/49983090718_71cfa75cfe_b.jpg',
+          like: '2',
+        },
+        {
+          id: 7,
+          name: '789',
+          price: 120,
+          url:
+            'https://live.staticflickr.com/65535/49983612971_a103f0385e_b.jpg',
+          like: '3',
+        },
+      ],
+      m: [
+        {
+          id: 1999,
+          name: 'Jacky',
+          price: 'Your Instagram Top Nine is ready!',
+          url:
+            'https://live.staticflickr.com/65535/50025256748_e67b905aa3_b.jpg',
+        },
+      ],
+    };
   }
 
   componentDidMount() {
@@ -19,23 +85,93 @@ class ProfilePage extends React.Component {
   }
 
   render() {
+    const unlikemeals = this.state.meals.filter((meal) => meal.like === '1');
+    const likemeals = this.state.meals.filter((meal) => meal.like === '2');
+    const verylikemeals = this.state.meals.filter((meal) => meal.like === '3');
     return (
       <View style={Styles.page}>
-        <Text>123456789</Text>
+        <View>
+          <Image style={styles.image1} source={{uri: this.state.m[0].url}} />
+          <Text style={styles.id}>{this.state.m[0].id}</Text>
+          <Text style={styles.mealName}>{this.state.m[0].name}</Text>
+          <Text style={styles.mealPrice}>{this.state.m[0].price}</Text>
+        </View>
         <ScrollableTabView>
-          <Text key="page1" title="a">
-            page1
-          </Text>
-          <Text key="page2" title="b">
-            page2
-          </Text>
-          <Text key="page2" title="c">
-            page3
-          </Text>
+          <View key="page1" title="a">
+            <ScrollView>
+              {unlikemeals.map((meal) => (
+                <MealItem key={meal.id} meal={meal} />
+              ))}
+            </ScrollView>
+          </View>
+          <View key="page2" title="b">
+            {likemeals.map((meal) => (
+              <MealItem key={meal.id} meal={meal} />
+            ))}
+          </View>
+          <View key="page2" title="c">
+            {verylikemeals.map((meal) => (
+              <MealItem key={meal.id} meal={meal} />
+            ))}
+          </View>
         </ScrollableTabView>
       </View>
     );
   }
 }
+
+function MealItem(props) {
+  return (
+    <View style={styles.mealItem}>
+      <Image style={styles.image} source={{uri: props.meal.url}} />
+      <View style={styles.mealContent}>
+        <Text style={styles.mealName}>{props.meal.name}</Text>
+        <Text style={styles.mealPrice}>${props.meal.price}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  id: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    paddingVertical: 2, // 文字上下留白
+  },
+  mealList: {
+    flex: 1,
+  },
+  mealItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderBottomColor: '#DDD',
+    borderBottomWidth: 1,
+    paddingVertical: 10,
+  },
+  image: {
+    width: 410,
+    height: 275,
+  },
+  image1: {
+    width: 150,
+    height: 150,
+    textAlign: 'center',
+    paddingVertical: 2, // 文字上下留白
+  },
+  mealContent: {
+    marginLeft: 10,
+  },
+  mealName: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    paddingVertical: 2, // 文字上下留白
+  },
+  mealPrice: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+});
 
 export default ProfilePage;
