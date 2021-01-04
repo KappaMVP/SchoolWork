788a94ecaa38c1f59f195999e840dca69c1a93b5
cookies <C107156108@nkust.edu.tw>
2021-01-04 19:37:08 +0800

    耘翰更新頁面幫忙丟、Notufy更新CSS、加一些功能

--- src/Pages/NotifyPage.js
+++ src/Pages/NotifyPage.js
@@ -1,41 +1,116 @@
 //通知頁
 import React from 'react';
-import {View, StyleSheet, Text, Image, ScrollView} from 'react-native';
+import {View, StyleSheet, Text, ScrollView} from 'react-native';
 import NotifyView from '../Views/NotifyView';
 import {notifyPageData as pageData} from '../data.source';
 class NotifyPage extends React.Component {
-  fakes = [
-    {
-      id: 1,
-      userName: '美國隊長',
-      status: 'Like your post.',
-      time: '2020-12-25 12:20',
-    },
-    {
-      id: 2,
-      userName: '奇異博士',
-      status: 'Leave a message on your post.',
-      time: '2020-12-26 13:20',
-    },
-    {
-      id: 3,
-      userName: '蜘蛛人',
-      status: 'Tagged you on other people post.',
-      time: '2020-12-27 14:20',
-    },
-    {
-      id: 4,
-      userName: '黑寡婦',
-      status: 'Leave a message on your post.',
-      time: '2020-12-28 15:20',
-    },
-    {
-      id: 5,
-      userName: '雷神索爾',
-      status: 'Like your post.',
-      time: '2020-12-29 16:20',
-    },
-  ];
+  constructor(props) {
+    super(props);
+    this.state = {
+      fakes: [
+        {
+          id: 1,
+          postId: 2,
+          userName: '美國隊長',
+          status: 'Like your post.',
+          time: '2020-12-25 12:20',
+        },
+        {
+          id: 2,
+          postId: 4,
+          userName: '奇異博士',
+          status: 'Leave a message on your post.',
+          time: '2020-12-26 13:20',
+        },
+        {
+          id: 3,
+          postId: 1,
+          userName: '蜘蛛人',
+          status: 'Tagged you on other people post.',
+          time: '2020-12-27 14:20',
+        },
+        {
+          id: 4,
+          postId: 10,
+          userName: '黑寡婦',
+          status: 'Leave a message on your post.',
+          time: '2020-12-28 15:20',
+        },
+        {
+          id: 5,
+          postId: 5,
+          userName: '雷神索爾',
+          status: 'Like your post.',
+          time: '2021-01-01 16:20',
+        },
+        {
+          id: 6,
+          postId: 7,
+          userName: '喵喵',
+          status: 'Like your post.',
+          time: '2021-01-02 16:20',
+        },
+        {
+          id: 7,
+          postId: 6,
+          userName: '汪汪',
+          status: 'Like your post.',
+          time: '2020-11-11 16:20',
+        },
+        {
+          id: 8,
+          postId: 3,
+          userName: '啾啾',
+          status: 'Like your post.',
+          time: '2020-11-30 16:20',
+        },
+      ],
+    };
+  }
+
+  //时间转换成刚刚，几分钟前，几小时之前，几天前等，常用于展示记录消息评论时间
+
+  // static dateToMsgTime(dataString) {
+  //   const dateTime = DateUtil.parserDateString(dataString);
+  //   const now = new Date();
+  //   const nowTimeSpan = now.getTime();
+  //   const oldTimeSpan = dateTime.getTime();
+  //   let result = '';
+  //   const milliseconds = nowTimeSpan - oldTimeSpan;
+  //   if (milliseconds <= 1000 * 60 * 1) {
+  //     result = '刚刚';
+  //   } else if (1000 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60) {
+  //     result = `${Math.round(milliseconds / (1000 * 60))}分钟前`;
+  //   } else if (
+  //     1000 * 60 * 60 * 1 < milliseconds &&
+  //     milliseconds <= 1000 * 60 * 60 * 24
+  //   ) {
+  //     result = `${Math.round(milliseconds / (1000 * 60 * 60))}小时前`;
+  //   } else if (
+  //     1000 * 60 * 60 * 24 < milliseconds &&
+  //     milliseconds <= 1000 * 60 * 60 * 24 * 2
+  //   ) {
+  //     result = `一天前${dataString.substr(11, 5)}`;
+  //   } else if (
+  //     1000 * 60 * 60 * 24 * 2 < milliseconds &&
+  //     milliseconds <= 1000 * 60 * 60 * 24 * 3
+  //   ) {
+  //     result = `两天前${dataString.substr(11, 5)}`;
+  //   } else {
+  //     result = dataString.substr(0, 10);
+  //   }
+  //   return result;
+  // }
+
+  //計算過去了多少天
+
+  //   function howData(mdata){
+  //     let d = new Date(mdata);
+  //     let reduce = new Date().getTime()-d.getTime()
+  //     return reduce/1000/3600/24
+  //  }
+
+  //  howData("2019-07-24")
 
   componentDidMount() {
     this.props.navigation.setParams({
@@ -43,22 +118,24 @@ class NotifyPage extends React.Component {
     });
   }
 
+  selectAvatar = () => {};
+
+  selectPost = () => {};
+
   render() {
     return (
       <View style={NPStyles.container}>
-        <ScrollView>
+        <ScrollView style={NPStyles.items}>
           <Text style={NPStyles.title}>本週</Text>
-          <View>
-            {this.fakes.map((fake) => (
-              <NotifyView key={fake.id} fake={fake} />
-            ))}
-          </View>
+          {this.state.fakes.map((fake) => (
+            <NotifyView key={fake.id} fake={fake} />
+          ))}
           <Text style={NPStyles.title}>本月</Text>
-          {this.fakes.map((fake) => (
+          {this.state.fakes.map((fake) => (
             <NotifyView key={fake.id} fake={fake} />
           ))}
           <Text style={NPStyles.title}>更早之前</Text>
-          {this.fakes.map((fake) => (
+          {this.state.fakes.map((fake) => (
             <NotifyView key={fake.id} fake={fake} />
           ))}
         </ScrollView>
@@ -73,24 +150,15 @@ const NPStyles = StyleSheet.create({
     backgroundColor: '#FFF', // 版面背景顏色
   },
   title: {
+    color: 'black',
+    backgroundColor: '#E0E0E0',
     textAlign: 'left', // 標題文字置中
     fontWeight: 'bold',
     fontSize: 20, // 標題文字大小
     padding: 10,
   },
-  divider: {
-    borderWidth: 0.5,
-    borderColor: '#DDDDDD',
-    paddingVertical: 10,
-  },
-  todoTitle: {
-    color: '#5661EC',
-    fontSize: 20, // 標題文字大小
-    fontWeight: 'bold', // 標題文字粗細
-    padding: 10, // 上下垂直內聚大小
-  },
-  todoItems: {
-    marginHorizontal: 10, // TodoItems 整個區塊的左右外距大小
+  items: {
+    borderBottomWidth: 1,
   },
 });
 
--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet, ScrollView} from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import {profilePageData as pageData} from '../data.source';
 import Styles from '../Styles/ProfilePage.style';
 import ScrollableTabView, {
@@ -10,41 +17,41 @@ class ProfilePage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      meals: [
+      Posts: [
         {
           id: 1,
-          name: '嫩煎魚排佐鮮蔬',
-          price: 350,
+          name: '吳天天',
+          content: 350,
           url:
             'https://live.staticflickr.com/65535/49983611666_f329a3c368_b.jpg',
           like: '1',
         },
         {
           id: 2,
-          name: '鮮蝦河粉',
-          price: 280,
+          name: '張和合',
+          content: 280,
           url:
             'https://live.staticflickr.com/65535/49997526092_91d7cf0963_b.jpg',
           like: '3',
         },
         {
           id: 3,
-          name: '清炒番茄義大利麵',
-          price: 180,
+          name: '陳踢踢',
+          content: 180,
           url:
-            'https://live.staticflickr.com/65535/49997272991_42d8072cf7_b.jpg',
+            'https://live.staticflickr.com/65535/49983091473_e09c8bb326_b.jpg',
           like: '2',
         },
         {
           id: 4,
-          name: '九層塔沙拉',
-          price: 120,
+          name: '王呵呵',
+          content: 120,
           url: 'https://i.imgur.com/Yg1t5sW.jpg',
         },
         {
           id: 5,
           name: '123',
-          price: 120,
+          content: 120,
           url:
             'https://live.staticflickr.com/65535/49983613426_29e5108012_b.jpg',
           like: '1',
@@ -52,7 +59,7 @@ class ProfilePage extends React.Component {
         {
           id: 6,
           name: '457',
-          price: 120,
+          content: 120,
           url:
             'https://live.staticflickr.com/65535/49983090718_71cfa75cfe_b.jpg',
           like: '2',
@@ -60,17 +67,30 @@ class ProfilePage extends React.Component {
         {
           id: 7,
           name: '789',
-          price: 120,
+          content: 120,
           url:
             'https://live.staticflickr.com/65535/49983612971_a103f0385e_b.jpg',
           like: '3',
         },
       ],
-      m: [
+      Profile: [
         {
           id: 1999,
+          name: 'Jacky  - photography',
+          content: 'Your Instagram Top Nine is ready!',
+          fans: '350粉絲',
+          follow: '480追蹤者',
+          post: '19篇貼文',
+          url:
+            'https://live.staticflickr.com/65535/50025256748_e67b905aa3_b.jpg',
+        },
+        {
+          id: 2000,
           name: 'Jacky',
-          price: 'Your Instagram Top Nine is ready!',
+          content: 'Your Instagram Top Nine is ready!',
+          fans: '350',
+          follow: '480',
+          post: '19',
           url:
             'https://live.staticflickr.com/65535/50025256748_e67b905aa3_b.jpg',
         },
@@ -85,63 +105,110 @@ class ProfilePage extends React.Component {
   }
 
   render() {
-    const unlikemeals = this.state.meals.filter((meal) => meal.like === '1');
-    const likemeals = this.state.meals.filter((meal) => meal.like === '2');
-    const verylikemeals = this.state.meals.filter((meal) => meal.like === '3');
+    const unlikePosts = this.state.Posts.filter((Post) => Post.like === '1');
+    const likePosts = this.state.Posts.filter((Post) => Post.like === '2');
+    const verylikePosts = this.state.Posts.filter((Post) => Post.like === '3');
+
     return (
       <View style={Styles.page}>
-        <View>
-          <Image style={styles.image1} source={{uri: this.state.m[0].url}} />
-          <Text style={styles.id}>{this.state.m[0].id}</Text>
-          <Text style={styles.mealName}>{this.state.m[0].name}</Text>
-          <Text style={styles.mealPrice}>{this.state.m[0].price}</Text>
+        <View style={Styles.List}>
+          <Text style={styles.post}>{this.state.Profile[0].post}</Text>
+          <Text style={styles.fans}>{this.state.Profile[0].fans}</Text>
+          <Text style={styles.follow}>{this.state.Profile[0].follow}</Text>
         </View>
+
+        <View style={Styles.List}>
+          <Image
+            style={styles.image1}
+            source={{uri: this.state.Profile[0].url}}
+          />
+        </View>
+
+        <View style={{flexDirection: 'column'}}>
+          <Text style={styles.id}>{this.state.Profile[0].id}</Text>
+          <Text style={styles.name}>{this.state.Profile[0].name}</Text>
+          <Text style={styles.Postcontent}>
+            {this.state.Profile[0].content}
+          </Text>
+        </View>
+
         <ScrollableTabView>
-          <View key="page1" title="a">
-            <ScrollView>
-              {unlikemeals.map((meal) => (
-                <MealItem key={meal.id} meal={meal} />
-              ))}
-            </ScrollView>
-          </View>
-          <View key="page2" title="b">
-            {likemeals.map((meal) => (
-              <MealItem key={meal.id} meal={meal} />
+          <ScrollView tabLabel="發布貼文">
+            {unlikePosts.map((Post) => (
+              <PostItem key={Post.id} Post={Post} />
             ))}
-          </View>
-          <View key="page2" title="c">
-            {verylikemeals.map((meal) => (
-              <MealItem key={meal.id} meal={meal} />
+          </ScrollView>
+          <ScrollView tabLabel="收藏貼文">
+            {likePosts.map((Post) => (
+              <PostItem key={Post.id} Post={Post} />
             ))}
-          </View>
+          </ScrollView>
+          <ScrollView tabLabel="標記貼文">
+            {verylikePosts.map((Post) => (
+              <PostItem key={Post.id} Post={Post} />
+            ))}
+          </ScrollView>
         </ScrollableTabView>
       </View>
     );
   }
 }
 
-function MealItem(props) {
+function PostItem(props) {
   return (
-    <View style={styles.mealItem}>
-      <Image style={styles.image} source={{uri: props.meal.url}} />
-      <View style={styles.mealContent}>
-        <Text style={styles.mealName}>{props.meal.name}</Text>
-        <Text style={styles.mealPrice}>${props.meal.price}</Text>
+    <View style={styles.PostItem}>
+      <Image style={styles.image} source={{uri: props.Post.url}} />
+      <View style={styles.PostContent}>
+        <Text style={styles.name}>{props.Post.name}</Text>
+        <Text style={styles.Postcontent}>{props.Post.content}</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  imagenew: {
+    width: 500,
+    height: 275,
+  },
+  new: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    paddingVertical: 3, // 文字上下留白
+  },
+  new1: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'left',
+    paddingVertical: 3, // 文字上下留白
+  },
+  new2: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    textAlign: 'right',
+    paddingVertical: 3, // 文字上下留白
+  },
+
+  new3: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#F00',
+  },
+
+  List: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   id: {
     fontSize: 14,
     fontWeight: 'bold',
     paddingVertical: 2, // 文字上下留白
+    marginLeft: 10,
   },
-  mealList: {
-    flex: 1,
-  },
-  mealItem: {
+  PostItem: {
     flexDirection: 'row',
     alignItems: 'center',
     borderBottomColor: '#DDD',
@@ -153,24 +220,49 @@ const styles = StyleSheet.create({
     height: 275,
   },
   image1: {
-    width: 150,
-    height: 150,
-    textAlign: 'center',
-    paddingVertical: 2, // 文字上下留白
+    flexDirection: 'row',
+    width: 100,
+    height: 100,
+    paddingVertical: 50, // 文字上下留白
   },
-  mealContent: {
+
+  PostContent: {
     marginLeft: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#F00',
   },
-  mealName: {
+  Postcontent: {
+    marginLeft: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#F00',
+  },
+  name: {
     fontSize: 14,
     fontWeight: 'bold',
     textAlign: 'left',
     paddingVertical: 2, // 文字上下留白
+    marginLeft: 10,
   },
-  mealPrice: {
+  follow: {
     fontSize: 16,
     fontWeight: 'bold',
     color: 'black',
+    marginLeft: 10,
+  },
+  fans: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'black',
+    marginLeft: 10,
+  },
+  post: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    paddingVertical: 3, // 文字上下留白
+    marginLeft: 10,
   },
 });
 
