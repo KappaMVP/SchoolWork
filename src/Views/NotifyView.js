6dfcc25a1e362dddd55ed6d1a5f48a90926f7b4d
cookies <C107156108@nkust.edu.tw>
2021-01-04 19:39:22 +0800

    更新CSS樣式

--- src/Views/NotifyView.js
+++ src/Views/NotifyView.js
@@ -3,77 +3,90 @@ import React from 'react';
 import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
 
 export default function NotifyView(props) {
+  const {
+    selectAvatar,
+    selectPost,
+    fake: {id, postId, userName, status, time},
+  } = props;
   return (
-    // <TouchableOpacity style={NVStyles.zone}>
-    //   <View style={NVStyles.content}>
-    //     <Image style={NVStyles.userImage} />
-    //     <Text style={NVStyles.name}>{props.fake.userName}</Text>
-    //     <Text style={NVStyles.status}>{props.fake.status}</Text>
-    //   </View>
-    //   <View>
-    //     <Text style={NVStyles.status}>{props.fake.time}</Text>
-    //   </View>
-    // </TouchableOpacity>
-    <TouchableOpacity style={NVStyles.content}>
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
+    <View style={NVStyles.zone}>
+      <TouchableOpacity
+        onPress={() => selectPost(postId)}
+        style={NVStyles.content}>
         <View>
-          <Image style={NVStyles.userImage} />
-        </View>
-        <View>
-          <View>
-            <View style={NVStyles.littleCon}>
-              <Text style={NVStyles.name}>{props.fake.userName}</Text>
-              <Text style={NVStyles.status}>{props.fake.status}</Text>
-            </View>
-            <View style={NVStyles.time}>
-              <Text>{props.fake.time}</Text>
-            </View>
-          </View>
-          <View>
+          <TouchableOpacity onPress={() => selectAvatar(id)}>
             <Image style={NVStyles.userImage} />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <View style={{width: 270}}>
+            <View style={NVStyles.littleCon}>
+              <TouchableOpacity onPress={() => selectAvatar(id)}>
+                <Text style={NVStyles.name}>{props.fake.userName}</Text>
+              </TouchableOpacity>
+              <Text style={NVStyles.status}>{props.fake.status}</Text>
+              <Text style={NVStyles.time}>{props.fake.time}</Text>
+            </View>
           </View>
         </View>
-      </View>
-    </TouchableOpacity>
+        <View>
+          <Image style={NVStyles.postImage} />
+        </View>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const NVStyles = StyleSheet.create({
   zone: {
-    borderBottomWidth: 2,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   content: {
-    padding: 5, // 區塊四周內距大小
+    paddingLeft: 5, // 區塊四周內距大小
+    paddingRight: 5,
+    paddingTop: 2,
+    paddingEnd: 2,
     flexDirection: 'row',
-    borderBottomWidth: 1,
+    borderBottomWidth: 0.5,
+    borderTopWidth: 0.5,
   },
   littleCon: {
-    width: 280,
     flexDirection: 'row',
+    flexWrap: 'wrap',
   },
   userImage: {
+    borderRadius: 5,
     margin: 10,
     flexDirection: 'row',
     width: 40,
     height: 40,
     backgroundColor: 'gray',
   },
+  postImage: {
+    borderRadius: 7,
+    margin: 5,
+    flexDirection: 'row',
+    width: 60,
+    height: 60,
+    backgroundColor: 'gray',
+  },
   name: {
-    fontSize: 20, // 標題文字大小
+    fontSize: 18, // 標題文字大小
     fontWeight: 'bold',
     textAlign: 'left',
+    marginTop: 5,
   },
   status: {
     fontSize: 18, // 子標題文字大小
     color: 'black', // 子標題文字顏色
     textAlign: 'left',
+    marginTop: 5,
+    marginEnd: 5,
   },
   time: {
-    paddingLeft: 1,
+    fontSize: 18,
+    color: '#ADADAD',
   },
 });
