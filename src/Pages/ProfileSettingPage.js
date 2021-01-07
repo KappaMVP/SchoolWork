60384ba3af973a87fd924c25196e0e2b4fbb2a0b
Kappa-MVP123 <c107156102@nkust.edu.tw>
2021-01-07 23:54:32 +0800

    hello wwwwwwwwwwwwwwwwwwww

--- src/Pages/ProfileSettingPage.js
+++ src/Pages/ProfileSettingPage.js
@@ -1,7 +1,16 @@
 //設定或設定>修改個人資料
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
 import {profileSettingPageData as pageData} from '../data.source';
+import TestImage from './test.jpg';
+import PageStyles from '../Styles/Page.style';
 
 class ProfileSettingPage extends React.Component {
   constructor() {
@@ -13,10 +22,85 @@ class ProfileSettingPage extends React.Component {
       title: pageData.title,
     });
   }
+  //變更大頭貼
+  HandleChangePhotoStiker() {}
+  //變更姓名
+  HandleChangeUserName() {}
+  //變更用戶名稱
+  HandleChangeUserID() {}
+  //變更個人簡介
+  HandleChangeIntroduction() {}
 
   render() {
-    return <View></View>;
+    return (
+      <View style={PageStyles.page}>
+        <View style={styles.imageView}>
+          <Image source={TestImage} style={styles.PhotoStiker} />
+          <TouchableOpacity onPress={() => this.HandleChangePhotoStiker}>
+            <Text style={styles.ChangePhotoStiker}>更換大頭貼照</Text>
+          </TouchableOpacity>
+        </View>
+        <View>
+          <View style={styles.item}>
+            <Text style={styles.label}>姓名</Text>
+            <TextInput
+              value=""
+              onValueChange={this.HandleChangeUserName()}
+              style={styles.textInput}
+            />
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.label}>用戶名稱</Text>
+            <TextInput
+              value=""
+              onValueChange={this.HandleChangeUserID()}
+              style={styles.textInput}
+            />
+          </View>
+          <View style={styles.item}>
+            <Text style={styles.label}>個人簡介</Text>
+            <TextInput
+              value=""
+              onValueChange={this.HandleChangeIntroduction()}
+              style={styles.textInput}
+            />
+          </View>
+        </View>
+      </View>
+    );
   }
 }
 
+const styles = StyleSheet.create({
+  imageView: {
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  PhotoStiker: {
+    width: 100,
+    height: 100,
+    borderRadius: 100,
+  },
+  ChangePhotoStiker: {
+    paddingVertical: 10,
+    color: 'blue',
+    fontWeight: 'bold',
+  },
+  item: {
+    height: 40,
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 5,
+    paddingHorizontal: 10,
+  },
+  textInput: {
+    flex: 1,
+    borderBottomWidth: 1,
+    borderBottomColor: '#C0C0C0',
+    marginLeft: 15,
+  },
+  label: {
+    fontWeight: 'bold',
+  },
+});
 export default ProfileSettingPage;
