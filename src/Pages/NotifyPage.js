91c2bb1209e7c183d4f68136efc1e16e0d51fd6b
cookies <C107156108@nkust.edu.tw>
2021-01-09 19:15:45 +0800

    不知道是不是元件化的東西

--- src/Pages/NotifyPage.js
+++ src/Pages/NotifyPage.js
@@ -1,7 +1,8 @@
 //通知頁
 import React from 'react';
-import {View, StyleSheet, Text, ScrollView} from 'react-native';
+import {View, Text, ScrollView} from 'react-native';
 import NotifyView from '../Views/NotifyView';
+import NPStyles from '../Styles/NotifyPage.style';
 import {notifyPageData as pageData} from '../data.source';
 class NotifyPage extends React.Component {
   constructor(props) {
@@ -68,50 +69,6 @@ class NotifyPage extends React.Component {
     };
   }
 
-  //时间转换成刚刚，几分钟前，几小时之前，几天前等，常用于展示记录消息评论时间
-
-  // static dateToMsgTime(dataString) {
-  //   const dateTime = DateUtil.parserDateString(dataString);
-  //   const now = new Date();
-  //   const nowTimeSpan = now.getTime();
-  //   const oldTimeSpan = dateTime.getTime();
-  //   let result = '';
-  //   const milliseconds = nowTimeSpan - oldTimeSpan;
-  //   if (milliseconds <= 1000 * 60 * 1) {
-  //     result = '刚刚';
-  //   } else if (1000 * 60 * 1 < milliseconds && milliseconds <= 1000 * 60 * 60) {
-  //     result = `${Math.round(milliseconds / (1000 * 60))}分钟前`;
-  //   } else if (
-  //     1000 * 60 * 60 * 1 < milliseconds &&
-  //     milliseconds <= 1000 * 60 * 60 * 24
-  //   ) {
-  //     result = `${Math.round(milliseconds / (1000 * 60 * 60))}小时前`;
-  //   } else if (
-  //     1000 * 60 * 60 * 24 < milliseconds &&
-  //     milliseconds <= 1000 * 60 * 60 * 24 * 2
-  //   ) {
-  //     result = `一天前${dataString.substr(11, 5)}`;
-  //   } else if (
-  //     1000 * 60 * 60 * 24 * 2 < milliseconds &&
-  //     milliseconds <= 1000 * 60 * 60 * 24 * 3
-  //   ) {
-  //     result = `两天前${dataString.substr(11, 5)}`;
-  //   } else {
-  //     result = dataString.substr(0, 10);
-  //   }
-  //   return result;
-  // }
-
-  //計算過去了多少天
-
-  //   function howData(mdata){
-  //     let d = new Date(mdata);
-  //     let reduce = new Date().getTime()-d.getTime()
-  //     return reduce/1000/3600/24
-  //  }
-
-  //  howData("2019-07-24")
-
   componentDidMount() {
     this.props.navigation.setParams({
       title: pageData.title,
@@ -128,38 +85,35 @@ class NotifyPage extends React.Component {
         <ScrollView style={NPStyles.items}>
           <Text style={NPStyles.title}>本週</Text>
           {this.state.fakes.map((fake) => (
-            <NotifyView key={fake.id} fake={fake} />
+            <NotifyView
+              key={fake.id}
+              fake={fake}
+              selectAvatar={this.selectAvatar()}
+              selectPost={this.selectPost()}
+            />
           ))}
           <Text style={NPStyles.title}>本月</Text>
           {this.state.fakes.map((fake) => (
-            <NotifyView key={fake.id} fake={fake} />
+            <NotifyView
+              key={fake.id}
+              fake={fake}
+              selectAvatar={this.selectAvatar()}
+              selectPost={this.selectPost()}
+            />
           ))}
           <Text style={NPStyles.title}>更早之前</Text>
           {this.state.fakes.map((fake) => (
-            <NotifyView key={fake.id} fake={fake} />
+            <NotifyView
+              key={fake.id}
+              fake={fake}
+              selectAvatar={this.selectAvatar()}
+              selectPost={this.selectPost()}
+            />
           ))}
         </ScrollView>
       </View>
     );
   }
 }
-
-const NPStyles = StyleSheet.create({
-  container: {
-    flex: 1, // 分割畫面區塊
-    backgroundColor: '#FFF', // 版面背景顏色
-  },
-  title: {
-    color: 'black',
-    backgroundColor: '#E0E0E0',
-    textAlign: 'left', // 標題文字置中
-    fontWeight: 'bold',
-    fontSize: 20, // 標題文字大小
-    padding: 10,
-  },
-  items: {
-    borderBottomWidth: 1,
-  },
-});
 
 export default NotifyPage;
