96b3183ae3ccfb182325231b52be74489c6c452c
cookies <C107156108@nkust.edu.tw>
2021-01-09 23:51:49 +0800

    改資料傳遞

--- src/Views/Elements/ResultRow.js
+++ src/Views/Elements/ResultRow.js
@@ -1,1 +1,36 @@
 //搜尋結果的那個元素
+import React from 'react';
+import {View, Text, TouchableOpacity, Image} from 'react-native';
+
+function ResultRow(props) {
+  const {
+    fake: {id, postId, userName, status, time},
+  } = props;
+  return (
+    <View>
+      <TouchableOpacity>
+        <View>
+          <TouchableOpacity>
+            <Image />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <View style={{width: 270}}>
+            <View>
+              <TouchableOpacity>
+                <Text>{userName}</Text>
+              </TouchableOpacity>
+              <Text>{status}</Text>
+              <Text>{time}</Text>
+            </View>
+          </View>
+        </View>
+        <View>
+          <Image />
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+export default ResultRow;
--- src/Views/NotifyView.js
+++ src/Views/NotifyView.js
@@ -1,39 +1,19 @@
 //通知的橫列
 import React from 'react';
-import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
+import {View, Text, Image, TouchableOpacity, ScrollView} from 'react-native';
 import NVStyles from '../Styles/NotifyView.style';
+import ResultRow from '../Views/Elements/ResultRow';
 
-export default function NotifyView(props) {
-  const {
-    selectAvatar,
-    selectPost,
-    fake: {id, postId, userName, status, time},
-  } = props;
+function NotifyView(props) {
+  const fakes = props.fakes;
+  const fakelist = fakes.map((fake) => {
+    fake;
+  });
   return (
     <View style={NVStyles.zone}>
-      <TouchableOpacity
-        onPress={() => selectPost(postId)}
-        style={NVStyles.content}>
-        <View>
-          <TouchableOpacity onPress={() => selectAvatar(id)}>
-            <Image style={NVStyles.userImage} />
-          </TouchableOpacity>
-        </View>
-        <View>
-          <View style={{width: 270}}>
-            <View style={NVStyles.littleCon}>
-              <TouchableOpacity onPress={() => selectAvatar(id)}>
-                <Text style={NVStyles.name}>{props.fake.userName}</Text>
-              </TouchableOpacity>
-              <Text style={NVStyles.status}>{props.fake.status}</Text>
-              <Text style={NVStyles.time}>{props.fake.time}</Text>
-            </View>
-          </View>
-        </View>
-        <View>
-          <Image style={NVStyles.postImage} />
-        </View>
-      </TouchableOpacity>
+      <ScrollView style={NVStyles.items}>{fakelist}</ScrollView>
     </View>
   );
 }
+
+export default NotifyView;
